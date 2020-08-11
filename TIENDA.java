5d61a2fc34bd61c17acf0faaf65810ff17f95568
AdonisSantillan03 <68193509+AdonisSantillan03@users.noreply.github.com>
2020-08-10 20:32:05 -0500

    Add files via upload

---
+++ TIENDA.java
@@ -0,0 +1,55 @@
+
+public class TIENDA {
+	private double codigo;
+    private float IVA;
+    public float precio;
+    private String nombreProducto;
+    
+
+    public Producto() {}
+
+    
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+    
+
+    public void setIVA(float IVA) {
+        this.IVA = IVA;
+    }  
+    
+    public float getIVA() {
+        return IVA;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+    
+    public double codigoProducto(){
+        codigo = (double)Math.floor(Math.random()*10000000);
+        return codigo;
+    }
+    
+    public float precioTotal(){
+        float total = ((getPrecio()*getIVA())+getPrecio());
+        return total;
+    }
+    /*
+    public float precioProducto(){
+        Random in = new Random();
+        int precio;
+        precio = in.nextInt(11);
+        return (float)precio;
+    }
+    */
+}
+
